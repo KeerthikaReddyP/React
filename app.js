a7f8a79f7ffa94b4af430b6326ed0cab78e48f75
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-04-28 17:06:15 +0530

    Complex HTML Structure

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "Hiii"),
-    React.createElement("p", {}, "This is a pragraph, sibling of h1 tag's Hiii")]
-  )
+  [
+    React.createElement(
+        "div",
+        { id: "child1" },
+        [React.createElement("h1", {}, "Hiii,I'm first child of child1"),
+        React.createElement("p", {}, "This is a pragraph, sibling of h1 tag's Hiii, second child of child1")]
+    ),
+    React.createElement(
+        "div",
+        { id: "child2" },
+        [React.createElement("h3", {}, "first child of child2"),
+        React.createElement("p", {}, "second child of child2,sibling of h3")]
+    )
+  ]
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
