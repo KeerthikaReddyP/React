651222e90197db2a98b0cb299716410da1dd3733
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-01 13:06:06 +0530

    Confusion

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-// Order of the script files do matter.
-
 const parent = React.createElement(
   "div",
   { id: "parent" },
@@ -18,23 +15,8 @@ const parent = React.createElement(
         React.createElement("p", {}, "second child of child2,sibling of h3")]
     )
   ]
-); 
-//This code is not so clean and understandable.
-//JSX is the solution.
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
-/*
-    Anything inside the div (whose id is root) will be 
-    replaced by this parent object.
-    REPLACED. Not appended.
-*/
-/*
-    Library Vs Framework
-
-    React can be applied to a small portion of our app
-    and it works independently.
-
-    Whereas framework comes with loads of things.
-*/
