e7a0bee59ddc4ca29a66946ca2cec8da9f67b247
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-02 22:04:47 +0530

    JSX

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{id:"heading"},"Namaste React👩‍🚀");
-console.log(heading);
+const heading = React.createElement( //Core of react to create element
+  "h1",
+  { id: "heading" },
+  "Namaste React👩‍🚀"
+);
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(heading);
+
+const jsxHeading=<h1 id="heading">Namaste React from JSX🚀</h1>; //JSX
+
+console.log(heading);
+console.log(jsxHeading);
+//Both are react elements
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
