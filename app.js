678f1df6d6b24cd818095a9100e17709e8933d3e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-02 15:56:05 +0530

    React.createElement (Core of react)

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement(
-        "div",
-        { id: "child1" },
-        [React.createElement("h1", {}, "Hiii,I'm first child of child1"),
-        React.createElement("p", {}, "This is a pragraph, sibling of h1 tag's Hiii, second child of child1")]
-    ),
-    React.createElement(
-        "div",
-        { id: "child2" },
-        [React.createElement("h3", {}, "first child of child2"),
-        React.createElement("p", {}, "second child of child2,sibling of h3")]
-    )
-  ]
-);
+const heading=React.createElement("h1",{id:"heading"},"Namaste React👩‍🚀");
+console.log(heading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
