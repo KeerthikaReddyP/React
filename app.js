c20e1a7b06c62406c21282db6d4588afe3fb0214
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-03 22:43:54 +0530

    React Functional Component

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React Element
 const heading = (
   <h1 id="heading" className="head">
     Namaste React from JSX🚀 
   </h1>
 );
-//Attribute names should be in camel case
-//Enclose JSX within round brackets () in case of multiple lines
 
-//Babel (JS compiler)(Transpiler)
+//React Components (1. Class based)(2. Functional)
+
+//React Functional Component
+//is a normal JS function
+//which returns some piece of JSX or React Element/elements
+//Starts with Capital letter
+const HeadingCOmponent=()=>{
+  return <h1>Namaste React from Functional Component</h1>;
+};
+
+const HeadCOmp=()=><h2>Huhuuuu</h2>;
+const HeadComp2=()=>(<h3>Heheee</h3>);
+//These are the same thing
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
