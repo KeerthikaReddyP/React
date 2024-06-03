61c9d62e4bf0bb486acbd81ae97d59e2f8c52dde
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-03 22:16:32 +0530

    Babel

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement( //Core of react to create element
-  "h1",
-  { id: "heading" },
-  "Namaste React👩‍🚀"
+const heading = (
+  <h1 id="heading" className="head">
+    Namaste React from JSX🚀 
+  </h1>
 );
+//Attribute names should be in camel case
+//Enclose JSX within round brackets () in case of multiple lines
 
-
-const jsxHeading=<h1 id="heading">Namaste React from JSX🚀</h1>; //JSX
-
-console.log(heading);
-console.log(jsxHeading);
-//Both are react elements
-
+//Babel (JS compiler)(Transpiler)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(heading);
