4dd86c61523f616aff80b09f69cfaf86edca828f
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-03 22:51:29 +0530

    Rendering Functional component

--- app.js
+++ app.js
@@ -8,19 +8,12 @@ const heading = (
   </h1>
 );
 
-//React Components (1. Class based)(2. Functional)
-
 //React Functional Component
-//is a normal JS function
-//which returns some piece of JSX or React Element/elements
-//Starts with Capital letter
 const HeadingCOmponent=()=>{
   return <h1>Namaste React from Functional Component</h1>;
 };
 
-const HeadCOmp=()=><h2>Huhuuuu</h2>;
-const HeadComp2=()=>(<h3>Heheee</h3>);
-//These are the same thing
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(heading); //For react elements
+root.render(<HeadingCOmponent />); //For functional components
