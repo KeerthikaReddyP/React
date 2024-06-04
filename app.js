500d1f31c606d8c3d01bd3d72c3f8f414831e921
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-04 15:49:04 +0530

    React component inside react element

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem=<span>React elememnt</span>;
+const elem = <span>React elememnt</span>;
 
-//React element inside a react element
-const title =(
-  <h1 id="heading" className="head">
-    {elem}
-    Namaste React from JSX🚀
-  </h1>
-);
-
-//React element inside Component
 const HeadingCOmponent = () => (
   <div>
-    {title}
     <h1>Namaste React from Functional Component</h1>
   </div>
 );
 
+//React component inside react element
+const title = (
+  <div>
+    <HeadingCOmponent />
+    <h1 id="heading" className="head">
+      {elem}
+      Namaste React from JSX🚀
+    </h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingCOmponent />);
+root.render(title);
