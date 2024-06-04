e8a644e58f19620f43fc1ae935033d0329c0c671
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-04 21:46:05 +0530

    Header component

--- app.js
+++ app.js
@@ -15,5 +15,28 @@ import ReactDOM from "react-dom/client";
  *    address
  *    contact info
  */
+const Header=()=>{
+  return <div className="header">
+    <div className="logo-container">
+      <img className="logo" src="https://tse2.mm.bing.net/th?id=OIP.ONdI50AcLDXgcVOR9WLdhwHaHa&pid=Api&P=0&h=180" />
+    </div>
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+};
+
+const AppComponent = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<AppComponent />);
