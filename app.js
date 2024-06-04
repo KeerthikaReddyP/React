8ef24187e1b5db0d99ac7d652e5f555422fcf4eb
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-04 21:23:34 +0530

    Starting our food delivery app

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = ()=>(
-  <div>
-    <h1 id="heading" className="head">
-      Namaste React from JSX🚀
-    </h1>
-  </div>
-);
-
-//3 ways to render a functional component
-const HeadingCOmponent = () => (
-  <div>
-    <Title />
-    <Title></Title>
-    {Title()}
-    <h1>Namaste React from Functional Component</h1>
-  </div>
-);
-
-
-
+/**
+ * Header
+ *    logo
+ *    nav items
+ * Body
+ *    Search bar
+ *    res container
+ *      res cards
+ * Footer
+ *    copyright
+ *    links
+ *    address
+ *    contact info
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(HeadingCOmponent);
+root.render();
