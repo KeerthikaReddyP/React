191b7f78c84692abeed70d6e343baa878b5296da
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-04 16:25:22 +0530

    Upto ep-03(Laying the foundation)

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span>React elememnt</span>;
-
-const HeadingCOmponent = () => (
+const Title = ()=>(
   <div>
-    <h1>Namaste React from Functional Component</h1>
-  </div>
-);
-
-//React component inside react element
-const title = (
-  <div>
-    <HeadingCOmponent />
     <h1 id="heading" className="head">
-      {elem}
       Namaste React from JSX🚀
     </h1>
   </div>
 );
 
+//3 ways to render a functional component
+const HeadingCOmponent = () => (
+  <div>
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1>Namaste React from Functional Component</h1>
+  </div>
+);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(title);
+root.render(HeadingCOmponent);
