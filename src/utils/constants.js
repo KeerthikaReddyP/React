a7b1a0eb54e44d2e98e8d183e4384f4d51cea452
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-05 16:37:03 +0530

    default export along with named export

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const LOGO_URL="https://tse2.mm.bing.net/th?id=OIP.ONdI50AcLDXgcVOR9WLdhwHaHa&pid=Api&P=0&h=180";
 
-export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export default CDN_URL;
