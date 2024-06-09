4fabc06ef2c447139ab2b4b3412b5910a9d18020
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-09 23:33:51 +0530

    Conditional rendering

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,26 +5,32 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  useEffect(()=>{fetchData()},[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const fetchData=async ()=>{
-    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.9692624&lng=79.59263039999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.9692624&lng=79.59263039999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    const jsonData=await data.json();
-    setListOfRestaurants(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    const jsonData = await data.json();
+    setListOfRestaurants(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
 
-  if(listOfRestaurants.length===0){
-    return <Shimmer />;
-  }
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search">
         <button
           onClick={() => {
-            setListOfRestaurants(listOfRestaurants.filter(
-              (res)=>(res.info.avgRating>=4.5)
-            ));
+            setListOfRestaurants(
+              listOfRestaurants.filter((res) => res.info.avgRating >= 4.5)
+            );
           }}
         >
           Filter high rated restaurants
