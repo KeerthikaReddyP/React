65f73dd94576ce543bb897c0591693a0fcecb7c8
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-11 21:06:52 +0530

    Children Routes

--- src/app.js
+++ src/app.js
@@ -3,29 +3,40 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Error from "./components/Error";
-import { createBrowserRouter , RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider , Outlet } from "react-router-dom";
 
 const AppComponent = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
-const appRouter=createBrowserRouter([
+const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<AppComponent />,
-    errorElement:<Error />,
+    path: "/",
+    element: <AppComponent />,
+    children:[
+      {
+        path:"/",
+        element:<Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
-  {
-    path:"/about",
-    element:<About />,
-  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);
