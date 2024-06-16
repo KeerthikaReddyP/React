cd991f1d840ec9d8905124c3090b53e9799fcd1b
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-16 23:06:48 +0530

    Link to

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
-import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnText, setBtnText] = useState("Login");
@@ -12,9 +12,9 @@ const Header = () => {
       </div>
       <div className="navbar">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
+          <li><Link to={"/"}>Home</Link></li>
+          <li><Link to={"/about"}>About us</Link></li>
+          <li><Link to={"/contact"}>Contact us</Link></li>
           <li>Cart</li>
         </ul>
         <button
