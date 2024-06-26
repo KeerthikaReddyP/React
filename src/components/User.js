ecff903f124415f078a5f548b822ba41f8a15262
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-26 07:30:29 +0530

    Updating state variables in Class-based Components

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
 const User = ({name,location}) => {
-    const [count]=useState(0);
-    const [count1]=useState(1);
+    const [count,setCount]=useState(0);
   return (
     <div className="user-div">
       <h2>Name : {name}</h2>
       <h2>Count: {count}</h2>
-      <h2>Count1: {count1}</h2>
       <h3>Location : {location}</h3>
       <h4>Contact : IDHELSDJ</h4>
     </div>
--- src/components/UserCass.js
+++ src/components/UserCass.js
@@ -13,11 +13,16 @@ class UserClass extends React.Component {
   render() {
     const { name,location } = this.props;
     const {count,count2}=this.state;
+
     return (
       <div className="user-div">
         <h2>Name : {name}</h2>
         <h2>Count : {count}</h2>
-        <h2>Count2 : {count2}</h2>
+        <button onClick={()=>{
+            this.setState({
+                count:this.state.count+1,
+            });
+        }}>Count increase</button>
         <h3>Location : {location}</h3>
         <h4>Contact : IDHELSDJ</h4>
       </div>
