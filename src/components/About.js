1893dc95575da4b0e247fd3c4e9b544e2dd4d501
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-27 12:45:56 +0530

    Lifecycle diagram/Order of the methods

--- src/components/About.js
+++ src/components/About.js
@@ -5,15 +5,15 @@ import UserClass from "./UserCass";
 class About extends React.Component{
     constructor(props){
         super(props);
-        // console.log("Parent Constructor");
+        console.log("Parent Constructor");
     }
 
     componentDidMount(){
-        // console.log("Parent componentDidMount");
+        console.log("Parent componentDidMount");
     }
 
     render(){
-        // console.log("Parent Render");
+        console.log("Parent Render");
         return (
             <div>
                 <h1>About us</h1>
--- src/components/UserCass.js
+++ src/components/UserCass.js
@@ -3,26 +3,29 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       userInfo: {
         name: "Dummy",
         location: "Dimmy",
       },
     };
-    // console.log(this.props.name+"Child Constructor");
+    console.log("Child Constructor");
   }
 
   async componentDidMount() {
-    // console.log(this.props.name+"Child componentDidMount");
+    console.log("Child componentDidMount");
     const data = await fetch("https://api.github.com/users/KeerthikaReddyP");
     const jsonData = await data.json();
     this.setState({ userInfo: jsonData });
   }
 
+  componentDidUpdate(){
+    console.log("Child componentDidUpdate");
+  }
+
   render() {
-    // console.log(this.props.name+"Child Render");
-    // const { name,location } = this.props;
+    console.log("Child Render");
     const { name, location } = this.state.userInfo;
     return (
       <div className="user-div">
