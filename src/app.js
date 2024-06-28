59764d4b7f8ecc06a0699f4b25ce3bbc42cbbd40
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-28 19:49:13 +0530

    Ep-10 starting (Jo diktha hai, vo bikta hai)

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
