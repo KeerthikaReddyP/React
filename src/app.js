7d106438513c041715d672c4424f088e5df6c9ae
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-28 19:32:51 +0530

    Lazy loading(About Us Page)

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-// import Grocery from "./components/Grocery";
 
 const Grocery = lazy(() => import("./components/Grocery"));
+const About=lazy(()=>import("./components/About"));
 
 const AppComponent = () => {
   return (
@@ -31,7 +31,7 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: <Suspense fallback={<h1>Wait...</h1>}><About /></Suspense>,
       },
       {
         path: "/contact",
