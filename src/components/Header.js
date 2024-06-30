e44f68eb40a95b6901d3f7c1e295c688ca488ff1
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-30 19:10:32 +0530

    Styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,27 +8,27 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="flex p-4 justify-between">
+    <div className="flex justify-between shadow-md">
       <div>
         <img className="w-32" src={LOGO_URL} />
       </div>
-      <div>
+      <div className="flex items-center">
         <ul className="flex">
-          <li className="m-4 p-2 items-center">{onlineStatus ? "🟢" : "🔴"}</li>
-          <li className="m-4 p-2 items-center">
+          <li className="m-4 p-2">{onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="m-4 p-2 hover:text-orange-500">
             <Link to={"/"}>Home</Link>
           </li>
-          <li className="m-4 p-2 items-center">
+          <li className="m-4 p-2 hover:text-orange-500">
             <Link to={"/about"}>About us</Link>
           </li>
-          <li className="m-4 p-2 items-center">
+          <li className="m-4 p-2 hover:text-orange-500">
             <Link to={"/contact"}>Contact us</Link>
           </li>
-          <li className="m-4 p-2 items-center">
+          <li className="m-4 p-2 hover:text-orange-500">
             <Link to={"/grocery"}>Grocery Store</Link>
           </li>
-          <li className="m-4 p-2 items-center">Cart</li>
-          <li className="m-4 p-2 items-center bg-purple-200">
+          <li className="m-4 p-2 ">Cart</li>
+          <li className="m-4 p-2 hover:text-orange-500">
             <button
               onClick={() => {
                 btnText === "Login"
