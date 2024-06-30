abb47bdb4744f9011853f56edd77de76425d0cc2
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-06-30 18:38:30 +0530

    RestaurantCard styling

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,12 +5,12 @@ const RestaurantCard = (props) => {
   const { cloudinaryImageId, name, cuisines, avgRating } = resData?.info;
   const { deliveryTime } = resData.info.sla;
   return (
-    <div className="res-card">
+    <div className="m-2 p-2 w-60 hover:border border-solid border-black">
       <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
-      <div className="res-name">{name}</div>
-      <div className="res-cuisines">{cuisines.join(", ")}</div>
-      <div className="res-details">
-        <div>☆ {avgRating}</div>
+      <div className="font-bold p-2">{name}</div>
+      <div className="p-2">{cuisines.join(", ")}</div>
+      <div className="flex justify-between p-2">
+        <div><span className="text-white bg-green-700 px-1 pb-1">☆</span> {avgRating}</div>
         <div>{deliveryTime} minutes</div>
       </div>
     </div>
