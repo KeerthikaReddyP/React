88b54a5969bc9c49797f0c4a9b01b5416d4b2fa4
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-03 23:17:35 +0530

    removed comments

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 const RestaurantCategory = ({ data,showItems,setShowIndex}) => {
   
   function clickHandler(){
-    // setShowItems(!showItems);
     setShowIndex();
   }
   return (
