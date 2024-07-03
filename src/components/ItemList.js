9ed8df3dfbc4aa1cba0db40a9297e4b4014b1aff
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-03 14:09:20 +0530

    Little css

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,13 +6,13 @@ const ItemList = ({ items }) => {
     <div>
       {items.map((item) => ( 
         <div key={item.card.info.id} className="flex justify-between my-2 p-4 border-gray-300 border-b-2">
-            <div className="flex flex-col text-left w-8/12">
+            <div className="flex flex-col text-left w-9/12">
                 <span className="text-lg">{item.card.info.name}</span>
                 <span className="text-lg">₹ {item.card.info.price/100}</span>
                 <p className="text-xs">{item.card.info.description}</p>
             </div>
-            <div className="w-4/12 flex flex-col items-center">
-                <img className="w-1/3" src={RESTAURANT_MENU_ITEM_IMAGE_URL+item.card.info.imageId}/>
+            <div className="w-3/12 flex flex-col items-center">
+                <img className="w-16 h-14" src={RESTAURANT_MENU_ITEM_IMAGE_URL+item.card.info.imageId}/>
                 <button className="px-4 bg-white text-green-500 font-bold rounded-lg shadow-lg">ADD</button>
             </div>
         </div>
