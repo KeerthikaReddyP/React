a285482028af60f71894625a14403e210ec40850
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-03 21:37:43 +0530

    Fixed image unavalaiblity

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,17 +3,32 @@ import { RESTAURANT_MENU_ITEM_IMAGE_URL } from "../utils/constants";
 const ItemList = ({ items }) => {
   return (
     <div>
-      {items.map((item) => ( 
-        <div key={item.card.info.id} className="flex justify-between my-2 p-4 border-gray-300 border-b-2">
-            <div className="flex flex-col text-left w-9/12">
-                <span className="font-semibold text-md">{item.card.info.name}</span>
-                <span className="font-semibold text-md">₹ {item.card.info.price?item.card.info.price/100:item.card.info.defaultPrice/100}</span>
-                <p className="text-xs">{item.card.info.description}</p>
-            </div>
-            <div className="w-3/12 flex flex-col place-items-end">
-                <img className="w-20 h-16" src={RESTAURANT_MENU_ITEM_IMAGE_URL+item.card.info.imageId}/>
-                <button className="px-4 bg-white text-green-600 font-bold rounded-lg shadow-lg">ADD</button>
-            </div>
+      {items.map((item) => (
+        <div
+          key={item.card.info.id}
+          className="flex justify-between my-2 p-4 border-gray-300 border-b-2"
+        >
+          <div className="flex flex-col text-left w-9/12">
+            <span className="font-semibold text-md">{item.card.info.name}</span>
+            <span className="font-semibold text-md">
+              ₹{" "}
+              {item.card.info.price
+                ? item.card.info.price / 100
+                : item.card.info.defaultPrice / 100}
+            </span>
+            <p className="text-xs">{item.card.info.description}</p>
+          </div>
+          <div className="w-3/12 flex flex-col place-items-end">
+            {item.card.info.imageId && (
+              <img
+                className="w-20 h-16"
+                src={RESTAURANT_MENU_ITEM_IMAGE_URL + item.card.info.imageId}
+              />
+            )}
+            <button className="px-4 bg-white text-green-600 font-bold rounded-lg shadow-lg">
+              ADD
+            </button>
+          </div>
         </div>
       ))}
     </div>
