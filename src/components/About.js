0e64d228d7517cb13142a2ae0049ea370205dd46
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-04 13:19:56 +0530

    context in Class-based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,26 @@
 import React from "react";
 import UserClass from "./UserCass";
+import UserContext from "../utils/UserContext";
 
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-class About extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>About us</h1>
-                <h3>We are from namaste React webseries</h3>
-                <UserClass name={"Keerthika "} location={"Delhi"}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>About us</h1>
+        <h3>We are from namaste React webseries</h3>
+        <div>
+            <UserContext.Consumer>
+                {(data)=>console.log(data)}
+            </UserContext.Consumer>
+        </div>
+        <UserClass name={"Keerthika "} location={"Delhi"} />
+      </div>
+    );
+  }
 }
 
 export default About;
