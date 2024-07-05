1ec49f0f2ec66948c2c9fa96ca7dbb9502023cdc
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-05 20:15:31 +0530

    Adding dummy items to cart's initialState

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    items: [],
+    items: ["Burger","Pizza"],
   },
   reducers: {
     addItem: (state, action) => {
