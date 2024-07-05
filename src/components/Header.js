b42eeef40a73629c24a2dea41cca1effd566283a
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-05 21:49:38 +0530

    console logging cart items

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,7 @@ const Header = () => {
   const { loggedInUser } = useContext(UserContext);
 
   const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems);
 
   return (
     <div className="flex justify-between shadow-md">
