e931a75bf16ecf3c713b8a0cadbb6282ede1942d
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-05 22:36:53 +0530

    Displaying real cart items on cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
+import ItemList from "./ItemList";
 
 const Cart = () => {
 
     const cartItems=useSelector((store)=>store.cart.items);
-    console.log(cartItems);
 
   return (
     <div className="text-center m-4 p-4">
       <h1>Cart</h1>
+      <ItemList items={cartItems}/>
     </div>
   );
 };
