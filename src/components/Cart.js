b4b8330d7cf9c6407cff5b66fa57576c63bd95c4
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-05 22:39:11 +0530

    styling cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,13 +2,14 @@ import { useSelector } from "react-redux";
 import ItemList from "./ItemList";
 
 const Cart = () => {
-
-    const cartItems=useSelector((store)=>store.cart.items);
+  const cartItems = useSelector((store) => store.cart.items);
 
   return (
     <div className="text-center m-4 p-4">
       <h1>Cart</h1>
-      <ItemList items={cartItems}/>
+      <div className="w-6/12 m-auto">
+        <ItemList items={cartItems} />
+      </div>
     </div>
   );
 };
