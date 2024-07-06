d579e07182475da5c2fd379fddd7b772ab2bc218
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-06 08:12:58 +0530

    showing clear cart only when there are cartItems

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,13 +14,8 @@ const Cart = () => {
   return (
     <div className="text-center m-4 p-4">
       <h1 className="font-bold">Cart</h1>
-      <button
-        className="m-2 py-2 px-4 text-white text-sm font-bold bg-green-500 rounded-lg"
-        onClick={handleClearCart}
-      >
-        Clear Cart
-      </button>
-      {cartItems.length === 0 && (
+
+      {cartItems.length === 0 ? (
         <div className="m-4 p-4">
           <h1 className="font-bold text-xl mb-2">Your cart is empty</h1>
           <h3 className="m-2">
@@ -32,7 +27,15 @@ const Cart = () => {
             </button>
           </Link>
         </div>
+      ) : (
+        <button
+          className="m-2 py-2 px-4 text-white text-sm font-bold bg-green-500 rounded-lg"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
       )}
+
       <div className="w-6/12 m-auto">
         <ItemList items={cartItems} />
       </div>
