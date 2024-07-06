44f857f98b5ef0d898f7476f278546a74fa61b1a
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-06 07:57:06 +0530

    clear cart button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,7 +6,8 @@ const Cart = () => {
 
   return (
     <div className="text-center m-4 p-4">
-      <h1>Cart</h1>
+      <h1 className="font-bold">Cart</h1>
+      <button className="m-2 py-2 px-4 text-white text-sm font-bold bg-green-500 rounded-lg">Clear Cart</button>
       <div className="w-6/12 m-auto">
         <ItemList items={cartItems} />
       </div>
