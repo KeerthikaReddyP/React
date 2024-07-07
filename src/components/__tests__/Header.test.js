460f631f98418f2c9251c6fe32ef149efb566111
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-07 22:20:42 +0530

    Wrapping eader component inside Provider component (Redux)

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,6 +1,12 @@
 const { render } = require("@testing-library/react");
 import Header from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
 
-it("should render Header component",()=>{
-    render(<Header />);
+it("should render Header component", () => {
+  render(
+    <Provider store={appStore}>
+      <Header />
+    </Provider>
+  );
 });
