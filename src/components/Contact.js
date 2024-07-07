c3119c80dc6a1a692b51e6c418164fb077cc0939
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-07 19:39:04 +0530

    Adding something to contact page for React testing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 const Contact=()=>{
     return (
-        <div>
-            <h1>Contact Us Page</h1>
+        <div className="m-4 p-4">
+            <h1 className="m-2 font-bold text-2xl">Contact Us Page</h1>
+            <form>
+                <input type="text" placeholder="name" className="border border-black m-4 p-2"/>
+                <input type="text" placeholder="message" className="border border-black m-4 p-2"/>
+                <button className="m-2 p-2 bg-blue-400 rounded-lg">Submit</button>
+            </form>
         </div>
     );
 };
