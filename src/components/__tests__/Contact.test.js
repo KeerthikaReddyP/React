3edd649d2b158afb2f3b52dafd2c377cb897e081
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-07 20:31:46 +0530

    describe (Grouping testcases)

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,39 +2,42 @@ import { render,screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Contact component should load",()=>{
-    //Rendering component onto jsdom
-    render(<Contact />);
-
-    //test whether heading is loaded or not
-    const heading=screen.getByRole("heading");
-
-    //Assertion
-    expect(heading).toBeInTheDocument();
+describe("Contact page's test cases",()=>{
+    test("Contact component should load",()=>{
+        //Rendering component onto jsdom
+        render(<Contact />);
+    
+        //test whether heading is loaded or not
+        const heading=screen.getByRole("heading");
+    
+        //Assertion
+        expect(heading).toBeInTheDocument();
+    });
+    
+    test("Contact page should have a button",()=>{
+        render(<Contact />);
+    
+        const button=screen.getByRole("button");
+    
+        expect(button).toBeInTheDocument();
+    });
+    
+    test("Contact page should have the test submit",()=>{
+        render(<Contact />);
+        const submitText=screen.getByText("Submit");
+        expect(submitText).toBeInTheDocument();
+    });
+    
+    test("Contact page should have two input boxes",()=>{
+        //Rendering
+        render(<Contact />);
+    
+        //Querying
+        const inputBoxes=screen.getAllByRole("textbox");
+        // console.log(inputBoxes);
+    
+        //Assertion
+        expect(inputBoxes.length).toBe(2);
+    });
 });
 
-test("Contact page should have a button",()=>{
-    render(<Contact />);
-
-    const button=screen.getByRole("button");
-
-    expect(button).toBeInTheDocument();
-});
-
-test("Contact page should have the test submit",()=>{
-    render(<Contact />);
-    const submitText=screen.getByText("Submit");
-    expect(submitText).toBeInTheDocument();
-});
-
-test("Contact page should have two input boxes",()=>{
-    //Rendering
-    render(<Contact />);
-
-    //Querying
-    const inputBoxes=screen.getAllByRole("textbox");
-    // console.log(inputBoxes);
-
-    //Assertion
-    expect(inputBoxes.length).toBe(2);
-});
