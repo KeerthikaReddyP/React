ef950ae52d971a7c222cfc507c5f2c99a4599c92
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-11 17:23:36 +0530

    Did some changes

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,29 +1,26 @@
 import CDN_URL from "../utils/constants";
-import { useContext } from "react";
-import UserContext from "../utils/UserContext";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
   const { cloudinaryImageId, name, cuisines, avgRating } = resData?.info;
   const { deliveryTime } = resData.info.sla;
 
-  const {loggedInUser}=useContext(UserContext);
-
   return (
-    <div className="m-2 p-2 w-60 h-96 hover:border border-solid border-black">
+    <div className="m-2 p-2 w-60 h-[400px] hover:border border-solid border-black">
       <img
         className="w-full h-48 object-cover"
         src={CDN_URL + cloudinaryImageId}
       />
       <div className="font-bold p-2">{name}</div>
-      <div className="p-2">{cuisines.join(", ")}</div>
-      <div className="flex justify-between p-2">
-        <div className="text-white bg-green-400 px-1 text-sm">
-          ☆ {avgRating}
+      <div className="flex flex-col justify-between" style={{height: "110px"}}>
+        <div className="p-2">{cuisines.join(", ")}</div>
+        <div className="flex justify-between p-2">
+          <div className="text-white bg-green-400 px-1 text-sm">
+            ☆ {avgRating}
+          </div>
+          <div>{deliveryTime} minutes</div>
         </div>
-        <div>{deliveryTime} minutes</div>
       </div>
-      <span>{loggedInUser}</span>
     </div>
   );
 };
@@ -32,7 +29,9 @@ export const withOpenLabel = (RestaurantCard) => {
   return (props) => {
     return (
       <div>
-        <label className="absolute m-2 ml-3 px-2 bg-gray-200 text-black">Open</label>
+        <label className="absolute m-2 ml-3 px-2 bg-gray-200 text-black">
+          Open
+        </label>
         <RestaurantCard {...props} />
       </div>
     );
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,7 @@
 const Shimmer=()=>{
     return <div>
-        <div className="m-4 p-4 flex flex-wrap">
+        <h1 className="bg-black text-white m-4 p-2 text-center">⚠️Please enable CORS extension to access swiggy API.</h1>
+        <div className="flex flex-wrap m-2 p-2 justify-center">
             <div className="m-4 p-2 w-1/6 h-96 bg-gray-200"></div>
             <div className="m-4 p-2 w-1/6 h-96 bg-gray-200"></div>
             <div className="m-4 p-2 w-1/6 h-96 bg-gray-200"></div>
